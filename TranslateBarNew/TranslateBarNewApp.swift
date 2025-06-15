f7242addaf3cccb4f9f2c65819597ca6398fbdc6
Sasha <savgorodniy@icloud.com>
2025-06-15 13:37:40 -0400

    Initial Commit

---
+++ TranslateBarNew/TranslateBarNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateBarNewApp.swift
+//  TranslateBarNew
+//
+//  Created by Sasha Fe on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct TranslateBarNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
